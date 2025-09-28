18184747758b96af926bc936fc9f3cbc6af90745
wangqikang <wangqikang@jargeelucky.com>
2025-09-28 08:28:23 +0800

    feat: add demos.

--- src/test/java/TestChat.java
+++ src/test/java/TestChat.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.jupiter.api.Test;
 
 public class TestChat {
+
+  /**
+   * 接入openai
+   */
+  @Test
+  void test01() {
+    ChatLanguageModel model = OpenAiChatModel.builder()
+        .apiKey("demo")
+        .modelName("gpt-4o-mini")
+        .build();
+
+    String answer = model.chat("你好，你是谁？");
+    System.out.println(answer);
+  }
+
+  /**
+   * 接入deepseek
+   */
+  @Test
+  void test02() {
+    ChatLanguageModel model = OpenAiChatModel.builder()
+        .baseUrl("https://api.deepseek.com")
+        .apiKey(System.getenv("DEEPSEEK_API_KEY"))
+        .modelName("gpt-4o-mini")
+        .build();
+
+    String answer = model.chat("你好，你是谁？");
+    System.out.println(answer);
+  }
 }
